423b70ec5500876b4b8c7d20011221cd7c9ceffa
sdcabral <sdcabral@sefaz.al.gov.br>
2022-08-01 14:01:00 -0300

    exercicio, questão 1

---
+++ Exercicio/src/main/java/chess/Factorial.java
@@ -0,0 +1,54 @@
+package chess;
+
+public class Factorial {
+    public static int factorial1(int fator) {
+        int result = 1;
+        int valor = fator;
+
+        while (valor >  1) {
+            result *= valor;
+            valor--;
+            System.out.println(result);
+        }
+        return result;
+    }
+    public static int factorial2(int fator){
+        int result = 1;
+        int valor = fator;
+
+        for (int i = 0; i <= valor; i++) {
+            result *= valor;
+            valor--;
+            System.out.println(result);
+        }
+        return result;
+    }
+    public static int factorial3(int fator){
+        int result = 1;
+        int valor = fator;
+
+        do {
+            result *= valor;
+            valor--;
+        }
+        while (valor >  1);
+            return result;
+    }
+    public static int factorial4(int fator){
+        int result = 1;
+        int valor = fator;
+
+        while(true) {
+            result *= valor;
+            valor--;
+            System.out.println(result);
+
+            if (valor < 1){
+                break;
+            }
+        }
+        return result;
+    }
+}
+
+//4*3*2*1.
---
+++ Exercicio/src/test/java/chess/TestFactorial.java
@@ -0,0 +1,28 @@
+package chess;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class TestFactorial {
+
+    @Test
+    public void factorial(){
+        assertEquals(120, Factorial.factorial1(5));
+        assertEquals(24, Factorial.factorial1(4));
+    }
+
+    @Test
+    public void factorialForFor(){
+        assertEquals(24, Factorial.factorial2(4));
+    }
+    @Test
+    public void factorialForDo(){
+        assertEquals(24, Factorial.factorial3(4));
+    }
+    @Test
+    public void factorialForTrue(){
+        assertEquals(24, Factorial.factorial4(4));
+    }
+}
