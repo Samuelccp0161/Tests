dc21056ef1e003692ca76b6effc8a930d468fbcb
sdcabral <sdcabral@sefaz.al.gov.br>
2022-08-19 13:15:39 -0300

    refatorando por conta que tive um problema no git, mas os testes estão todos passando só falta refatorar algumas coisas. hoje dia 18 de agosto refatorei metade do código praticamente.

--- Exercicio/src/test/java/BlowsUpTest.java
+++ Exercicio/src/test/java/BlowsUpTest.java
@@ -2,12 +2,38 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-//public class BlowsUpTest {
-//    @Test
-//    public void blowsUpTest(){
-//        try{
-//            fail("Somebody should catch this!");
-//        }
-//        catch ()
-//    }
-//}
+public class BlowsUpTest {
+    @Test
+    public void blowsUpTest(){
+        try{
+            blowsUp();
+            fail();
+        }
+        catch (RuntimeException success){
+            assertEquals("Somebody should catch this!", success.getMessage());
+        }
+    }
+    public void blowsUp(){
+        throw new RuntimeException("Somebody should catch this!");
+    }
+    @Test
+    public void rethrowsTest(){
+        try {
+            rethrows();
+            fail();
+        }
+        catch (RuntimeException exception){
+//            success.getCause().getMessage();
+            assertEquals("Somebody should catch this!", exception.getCause().getMessage());
+//            exception.printStackTrace();
+        }
+    }
+    public void rethrows(){
+        try {
+            blowsUp();
+        }
+        catch (RuntimeException exception){
+
+        }
+    }
+}
