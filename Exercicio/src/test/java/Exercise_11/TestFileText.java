a47223386a487f24afb1d93a08436f59e45db23f
sdcabral <sdcabral@sefaz.al.gov.br>
2022-10-26 10:51:42 -0300

    inicio da 1 questao da lição 11

--- Exercicio/src/test/java/Exercise_11/TestFileText.java
+++ Exercicio/src/test/java/Exercise_11/TestFileText.java
@@ -6,9 +6,6 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 public class TestFileText {
 
@@ -17,7 +14,7 @@ public class TestFileText {
 
     }
 
-    public void writeFile(String text, String filename) throws IOException{
+    public void writeFile(String text, String filename) throws IOException {
         FileWriter writer = null;
         try {
             writer = new FileWriter(filename);
@@ -28,6 +25,7 @@ public class TestFileText {
                 writer.close();
         }
     }
+
     public String read(String filename) throws IOException {
         BufferedReader reader = null;
         StringBuilder builder = new StringBuilder();
@@ -39,7 +37,8 @@ public class TestFileText {
                     break;
                 }
                 builder.append((char) ch);
-            } return builder.toString();
+            }
+            return builder.toString();
         } finally {
             if (reader != null)
                 reader.close();
@@ -51,7 +50,7 @@ public class TestFileText {
     public void testFileText() throws IOException {
         String filename = "text.txt";
         String text = "1. Create a test to write the text of this exercise to the file system. The test should read the\n" +
-                "file back in and make assertions about the content. Ensure that you can run the test\n" +
+                "fiteFile(String ile back in and make assertions about the content. Ensure that you can run the test\n" +
                 "multiple times and have it pass. Finally, make sure that there are no leftover files when the\n" +
                 "test finishes, even if an exception is thrown.";
         try {
@@ -64,5 +63,16 @@ public class TestFileText {
 
     }
 
-
+    @Test
+    public void testTimeBuffered() throws Exception {
+        File file = new File("fofofca.txt");
+        writeFile("a", "fofofca.txt");
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file));
+        bufferedWriter.write("a");
+        for (int i = 0; i <= file.length(); i++) {
+            bufferedWriter.write("1");
+            bufferedWriter.flush();
+            file.delete();
+        }
+    }
 }
