3ffc3b97e4297c2a56688dc0f3c02bc96fec1198
pedrojjsantos <pedrojjsantos.cc@gmail.com>
2023-04-25 18:21:29 -0300

    ring,termino do next e previous, falta fazer o remote.

--- Exercicio/src/main/java/Exercise_14/Ring.java
+++ Exercicio/src/main/java/Exercise_14/Ring.java
@@ -2,12 +2,25 @@ package Exercise_14;
 
 public class Ring {
     Node current;
-    Node ff;
+    public int size;
+
     public void add(int number){
-        current = new Node(number);
-        if (current.value != 0){
+        if(size == 0){
+            current = new Node(number);
+            current.next = current;
+            current.prev = current;
+        } else{
+            Node node = new Node(number);
+            node.next = current.next;
+            current.next = node;
+
+            node.prev = current;
+            node.next.prev = node;
+
+            next();
         }
-        current.next = current;
+        size++;
+
 
     }
     public int get(){
@@ -16,12 +29,32 @@ public class Ring {
     public void next(){
         current = current.next;
     }
+    public void prev(){
+        current = current.prev;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void remove(){
+        Node nextNode = current.next;
+        Node prevNode = current.prev;
+
+        nextNode.prev = prevNode;
+        prevNode.next = nextNode;
+
+        current = prevNode;
+
+    }
     private static class Node {
-        public Node next;
+        private Node next;
+        private Node prev;
         public int value;
         public Node(int val){
             this.value = val;
             next = null;
+            prev = null;
         }
     }
 }
--- Exercicio/src/test/java/Exercise_14/RingTest.java
+++ Exercicio/src/test/java/Exercise_14/RingTest.java
@@ -5,19 +5,12 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class RingTest {
+
     @Test
-    public void ringAddTest() {
+    public void createRingTest() {
         Ring ring = new Ring();
 
-        int number = 4;
-
-        ring.add(number);
-
-        assertEquals(4, ring.get());
-
-        ring.next();
-
-        assertEquals(4, ring.get());
+        assertEquals(0, ring.size());
     }
 
     @Test
@@ -26,19 +19,75 @@ public class RingTest {
 
         int number1 = 4;
         int number2 = 8;
+        int number3 = 12;
 
         ring.add(number1);
+        assertEquals(1, ring.size());
+        assertEquals(number1, ring.get());
+
         ring.add(number2);
+        assertEquals(2, ring.size());
+        assertEquals(number2, ring.get());
 
-        assertEquals(8, ring.get());
+        ring.add(number3);
+        assertEquals(3, ring.size());
+        assertEquals(number3, ring.get());
+    }
 
+    @Test
+    public void testNext() {
+        Ring ring = new Ring();
+
+        ring.add(2);
+        ring.add(1);
+        ring.add(3);
+
+        assertEquals(3, ring.get());
         ring.next();
-
-        assertEquals(4, ring.get());
-
+        assertEquals(2, ring.get());
         ring.next();
+        assertEquals(1, ring.get());
+        ring.next();
+        assertEquals(3, ring.get());
 
-        assertEquals(8, ring.get());
+    }
 
+    @Test
+    public void testPrevious() {
+        Ring ring = new Ring();
+
+        ring.add(1);
+        ring.add(2);
+        ring.add(3);
+
+        assertEquals(3, ring.get());
+        ring.prev();
+        assertEquals(2, ring.get());
+        ring.prev();
+        assertEquals(1, ring.get());
+        ring.prev();
+        assertEquals(3, ring.get());
+
+    }@Test
+    public void testRemove() {
+        Ring ring = new Ring();
+
+        ring.add(1);
+        ring.add(2);
+        ring.add(3);
+
+        ring.remove();
+
+        assertEquals(1, ring.get());
+        ring.prev();
+        assertEquals(2, ring.get());
+        ring.prev();
+        assertEquals(1, ring.get());
+
+//        assertEquals(1, ring.get());
+//        ring.next();
+//        assertEquals(2, ring.get());
+//        ring.next();
+//        assertEquals(1, ring.get());
     }
 }
