80d7e0844b42187b09eec9b31c031a05a1b9fc74
pedrojjsantos <pedrojjsantos.cc@gmail.com>
2023-06-06 16:58:19 -0300

    inicio da questão 3.

--- Exercicio/src/main/java/Exercise_15/ToStringer.java
+++ Exercicio/src/main/java/Exercise_15/ToStringer.java
@@ -1,18 +1,34 @@
 package Exercise_15;
 
 import java.lang.reflect.Field;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class ToStringer {
-    public static String name;
 
     public static String getDump(Object obj) throws Exception {
-//        Field[] fields = obj.getClass().getDeclaredFields();
-//        fields.
-        return "";
-    }
+        Field[] fields = obj.getClass().getDeclaredFields();
+        List<Field> fieldsWithDump = new ArrayList<>();
 
+        for (Field field : fields){
+            if (field.getAnnotation(Dump.class) != null)
+                fieldsWithDump.add(field);
+        }
+
+        for (int i = 0; i < fieldsWithDump.size(); i++){
+        // estudar collections.sort que é algo que eu posso usar para ordenar.
+        }
+
+        List<String> values = new ArrayList<>();
+
+        for (Field field : fieldsWithDump) {
+            String name = field.getName();
+            Object value = field.get(obj);
+
+            values.add(name + " = " + value);
+        }
+
+        return String.join(", ", values);
+    }
 }
-//criar uma classe chamado ToStringer, criar uma anotação @Dump que pode ser colocada em qualquer campo em uma classe,
-//
--- Exercicio/src/test/java/Exercise_15/ToStringerTest.java
+++ Exercicio/src/test/java/Exercise_15/ToStringerTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class ToStringerTest {
+
     @Test
     public void testEmptyDump() throws Exception {
         //vou passsar uma objeto(qualquer objeto) aonde nao tenha nenhuma anotação @dump.
@@ -18,14 +19,40 @@ public class ToStringerTest {
         OneDump oneDump = new OneDump();
         String dump = ToStringer.getDump(oneDump);
         assertEquals("str = 1", dump);
+    }
 
+    @Test
+    public void testMultipleDump() throws Exception {
+        MultipleDump multipleDump = new MultipleDump();
+        String dump = ToStringer.getDump(multipleDump);
+        assertEquals("str1 = 1, str2 = 2", dump);
+    }
+
+    @Test
+    public void testOrderDump() throws Exception {
+        OrderDump orderDump = new OrderDump();
+        String dump = ToStringer.getDump(orderDump);
+        assertEquals("str2 = 2, str1 = 1", dump);
     }
 
     static class EmptyDump{
-        String str = "1";
+        String str = "2";
     }
     static class OneDump{
         @Dump
         String str = "1";
     }
+    static class MultipleDump{
+        @Dump
+        String str1 = "1";
+        @Dump
+        String str2 = "2";
+        String str3 = "3";
+    }static class OrderDump{
+        @Dump
+        String str1 = "1";
+        @Dump(order = 1)
+        String str2 = "2";
+    }
+
 }
